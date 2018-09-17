ea35b72ba306922204a0c96de3b6f622eb91b3ef
Jugnu Agrawal <jugnuagrawal.brjn@gmail.com>
2018-09-17 22:45:40 +0530

    json response

--- index.js
+++ index.js
@@ -8,16 +8,17 @@ module.exports = function (options = { url: '' }) {
     return (req, res, next) => {
         if (req.headers.authorization) {
             request.get({
-                url: options.url+'/validate',
+                url: options.url + '/validate',
                 headers: {
                     'authorization': req.headers.authorization
-                }
+                },
+                json: true
             }, function (err, authRes, body) {
                 if (err) {
                     res.status(401).json({ message: 'You are not authorized to access this.' });
                     return;
                 }
-                if (authRes.statusCode !== 200 || authRes.statusCode !== 202 || authRes.statusCode !== 204) {
+                if (authRes.statusCode !== 200 && authRes.statusCode !== 202 && authRes.statusCode !== 204) {
                     res.status(authRes.statusCode).json(body);
                 } else {
                     next();
