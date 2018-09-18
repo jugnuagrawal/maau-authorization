0a645f1729e3d036e692f0622ba193b091882e5a
Jugnu Agrawal <jugnuagrawal.brjn@gmail.com>
2018-09-18 23:35:06 +0530

    user details added

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const request = require('request');
-module.exports = function (options = { url: '' }) {
+module.exports = function (options = { url: '', operation: '' }) {
     if (!options || !options.url) {
         options = {
-            url: 'http://maau.jugnuagrawal.in'
+            url: 'http://maau.jugnuagrawal.in',
+            operation: null
         };
     }
     return (req, res, next) => {
         if (req.headers.authorization) {
+            const reqURL = options.url + '/authorized';
+            if (options.operation) {
+                reqURL += ('?operation=' + options.operation);
+            }
             request.get({
-                url: options.url + '/validate',
+                url: reqURL,
                 headers: {
                     'authorization': req.headers.authorization
                 },
@@ -21,6 +26,10 @@ module.exports = function (options = { url: '' }) {
                 if (authRes.statusCode !== 200 && authRes.statusCode !== 202 && authRes.statusCode !== 204) {
                     res.status(authRes.statusCode).json(body);
                 } else {
+                    req.userDetails = body;
+                    if(req.method == 'POST' || req.method == 'PUT' || req.method == 'DELETE'){
+                        req.body.manageByUser = body._id;
+                    }
                     next();
                 }
             });
