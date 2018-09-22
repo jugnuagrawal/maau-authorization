2f512885a818334f3baf5fe0ae5f1137f6fe09c6
Jugnu Agrawal <jugnuagrawal.brjn@gmail.com>
2018-09-22 19:46:20 +0530

    added namespace and options

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const request = require('request');
-module.exports = function (options = { url: '', operation: '' }) {
+module.exports = function (options = { url: '', options: '' }) {
     if (!options || !options.url) {
         options = {
             url: 'http://maau.jugnuagrawal.in',
-            operation: null
+            options: null
         };
     }
     return (req, res, next) => {
         if (req.headers.authorization) {
             const reqURL = options.url + '/authorized';
-            if (options.operation) {
-                reqURL += ('?operation=' + options.operation);
+            if (options.options) {
+                reqURL += ('?options=' + options.options);
             }
             request.get({
                 url: reqURL,
@@ -27,8 +27,9 @@ module.exports = function (options = { url: '', operation: '' }) {
                     res.status(authRes.statusCode).json(body);
                 } else {
                     req.userDetails = body;
-                    if(req.method == 'POST' || req.method == 'PUT' || req.method == 'DELETE'){
+                    if (req.method == 'POST' || req.method == 'PUT' || req.method == 'DELETE') {
                         req.body.manageByUser = body._id;
+                        req.body.namespace = body.data.namespace;
                     }
                     next();
                 }
